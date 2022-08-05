ed976dcf725565b0d7a3d9832282169393d0609c
Desipeli <93584995+Desipeli@users.noreply.github.com>
2022-08-05 10:47:59 +0300

    2.15 ja 2.16

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import AddForm from './components/AddForm'
 import ShowPersons from './components/ShowPersons'
 import Filter from './components/Filter'
+import Numbers from './services/Numbers'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
@@ -10,14 +11,36 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [filterText, setFilterText] = useState('')
 
+
   const getNumbers = () => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-      })
+    Numbers
+      .getAll()
+      .then(response => setPersons(response))
   }
   useEffect(getNumbers, [])
+
+  const addName = (event) => {
+    event.preventDefault()
+     if (persons.find((person) => person.name.toLowerCase() === newName.toLowerCase())) {
+       alert(` ${newName} is already added to phonebook`)
+       return
+     }
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+    }
+    Numbers
+        .create(newPerson)
+        .then(returnedPerson => {
+          setPersons(persons.concat(newPerson))
+          setNewName('')
+          setNewNumber('')
+        })
+     }
+
+  const filterHandler = (event) => {
+    setFilterText(event.target.value)
+  }
 
   const handleNameFieldChange = (event) => {
     setNewName(event.target.value)
@@ -26,28 +49,7 @@ const App = () => {
   const handleNumberFieldChange = (event) => {
     setNewNumber(event.target.value)
   }
-
-  const addName = (event) => {
-    event.preventDefault()
-    if (persons.find((person) => person.name === newName)) {
-      alert(` ${newName} is already added to phonebook`)
-      return
-    }
-    const newPerson = {
-      name: newName,
-      number: newNumber,
-    }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
-  }
-
-  const filterHandler = (event) => {
-    setFilterText(event.target.value)
-  }
-
   
-
   return (
     <div>
       <h2>Phonebook</h2>
